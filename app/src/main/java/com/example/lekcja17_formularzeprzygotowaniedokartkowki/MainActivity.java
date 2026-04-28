50e8b254107e1bc1ae72df3e36eab480a41fec6f
Kajerkooo2 <prokajerko79@gmail.com>
2026-04-28 08:32:01 +0200

    Inicializacja elementow w MainActivity

--- app/src/main/java/com/example/lekcja17_formularzeprzygotowaniedokartkowki/MainActivity.java
+++ app/src/main/java/com/example/lekcja17_formularzeprzygotowaniedokartkowki/MainActivity.java
@@ -27,6 +27,21 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText editTitle, editAuthor, editPrice;
+    private Spinner spinnerGenre;
+    private Switch switchNew;
+    private SeekBar seekBarPromo;
+    private TextView textPromoValue;
+    private CheckBox checkPaper, checkMobi, checkAudiobook, checkPdf;
+    private RadioButton radio18, radio12, radio9, radio0;
+    private RadioGroup radioGroupAge, radioGroupAge2;
+    private Button buttonSubmit;
+    private ListView listViewBooks;
+
+    private List<Book> bookList = new ArrayList<>();
+    private BookAdapter bookAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,5 +52,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    private void initializeViews() {
+        editTitle = findViewById(R.id.etTytul);
+        editAuthor = findViewById(R.id.etAutor);
+        editPrice = findViewById(R.id.etCena);
+        spinnerGenre = findViewById(R.id.spinnerGenre);
+        switchNew = findViewById(R.id.switchCzNowa);
+        seekBarPromo = findViewById(R.id.seekBarPromocja);
+        textPromoValue = findViewById(R.id.tvPromoPercent);
+        checkPaper = findViewById(R.id.cbPapier);
+        checkMobi = findViewById(R.id.cbMobi);
+        checkAudiobook = findViewById(R.id.cbAudiobook);
+        checkPdf = findViewById(R.id.cbPdf);
+        radioGroupAge = findViewById(R.id.radioGroupAge);
+        radioGroupAge2 = findViewById(R.id.radioGroupAge2);
+        radio18 = findViewById(R.id.radio18);
+        radio12 = findViewById(R.id.radio12);
+        radio9 = findViewById(R.id.radio9);
+        radio0 = findViewById(R.id.radio0);
+        buttonSubmit = findViewById(R.id.btnWyslij);
+        listViewBooks = findViewById(R.id.lvBooks);
     }
 }
